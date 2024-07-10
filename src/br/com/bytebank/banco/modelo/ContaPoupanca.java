d38c74ade82066f441a65c82047359744b09d915
sbm-waltersouza <169094760+sbm-waltersouza@users.noreply.github.com>
2024-07-10 12:06:07 -0300

    O método toString()

--- src/br/com/bytebank/banco/modelo/ContaPoupanca.java
+++ src/br/com/bytebank/banco/modelo/ContaPoupanca.java
@@ -8,7 +8,13 @@ public class ContaPoupanca extends Conta {
 
 	@Override
 	public void deposita(double valor) {
-		super.saldo += valor;	
+		super.saldo += valor;
 	}
-	
+
+	// ...
+	@Override
+	public String toString() {
+		return "ContaPoupanca, " + super.toString();
+	}
+	// ...
 }
